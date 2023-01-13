d8439243da6bfca95e14ddb71d338530a6d01122
kaduh15 <kadu.siva2014@gmail.com>
2023-01-13 18:17:05 -0300

    Feat:✳ Save the IP in the token

--- app/backend/src/modules/login/login.controller.ts
+++ app/backend/src/modules/login/login.controller.ts
@@ -1,14 +1,17 @@
+import { hashSync } from 'bcrypt';
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { CreateLoginDto } from './dto/create-login.dto';
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
-  async login(@Body() createLoginDto: CreateLoginDto) {
-    const token = await this.loginService.login(createLoginDto);
+  async login(@Body() createLoginDto: CreateLoginDto, @RealIP() ip: string) {
+    const ipHash = hashSync(ip.split(':')[ip.split(':').length - 1], 10);
+    const token = await this.loginService.login(createLoginDto, ipHash);
 
     return { token };
   }
